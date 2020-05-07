870112380588d74931161f74d204dc40a7ef9757
Angelica Velez <anvelez26@gmail.com>
2020-05-07 17:09:17 -0500

    modified startup to fix db port issues

--- TheGuardianAPI/TheGuardianAPI/Startup.cs
+++ TheGuardianAPI/TheGuardianAPI/Startup.cs
@@ -1,89 +1,96 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using TheGuardian.Core.Interfaces;
 using TheGuardian.DataAccess;
 
-namespace TheGuardianAPI
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+[assembly : ApiController]
+namespace TheGuardianAPI {
+    public class Startup {
+        private const string CorsPolicyName = "AllowConfiguredOrigins";
+
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddApplicationInsightsTelemetry();
+        public void ConfigureServices (IServiceCollection services) {
+            // switch between database providers using runtime configuration
+            // (the existing migrations are SQL-Server-specific, but the model itself is not)
 
-            services.AddDbContext<GuardianContext>(options => options.UseNpgsql(Configuration.GetConnectionString("GuardianDbPostgreSqlDockerDesktop")));
+            // this should be the name of a connection string.
+            string whichDb = Configuration["DatabaseConnection"];
+            if (whichDb is null) {
+                throw new InvalidOperationException ($"No value found for \"DatabaseConnection\"; unable to connect to a database.");
+            }
 
-            services.AddScoped<IGuardianRepository, GuardianRepository>();
+            string connection = Configuration.GetConnectionString (whichDb);
+            if (connection is null) {
+                throw new InvalidOperationException ($"No value found for \"{whichDb}\" connection; unable to connect to a database.");
+            }
 
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Guardian API", Version = "v1" });
+            if (whichDb.Contains ("PostgreSql", StringComparison.InvariantCultureIgnoreCase)) {
+                services.AddDbContext<GuardianContext> (options =>
+                    options.UseNpgsql (connection));
+            } else {
+                services.AddDbContext<GuardianContext> (options =>
+                    options.UseSqlServer (connection));
+            }
+
+            services.AddScoped<IGuardianRepository, GuardianRepository> ();
+
+            services.AddSwaggerGen (c => {
+                c.SwaggerDoc ("v1", new OpenApiInfo { Title = "Guardian API", Version = "v1" });
             });
 
-            services.AddCors(options =>
-            {
-                options.AddPolicy("AllowLocalAndAppServiceAngular", builder =>
-                   builder.WithOrigins("http://localhost:4200")
-                   .AllowAnyMethod()
-                   .AllowAnyHeader());
+            // support switching between database providers using runtime configuration
+
+            var allowedOrigins = Configuration.GetSection ("CorsOrigins").Get<string[]> ();
+
+            services.AddCors (options => {
+                options.AddPolicy (CorsPolicyName, builder =>
+                    builder.WithOrigins (allowedOrigins ?? Array.Empty<string> ())
+                    .AllowAnyMethod ()
+                    .AllowAnyHeader ()
+                    .AllowCredentials ());
             });
 
-            services.AddControllers(options =>
-            {
-                options.OutputFormatters.RemoveType<StringOutputFormatter>();
+            services.AddControllers (options => {
+                // remove the default text/plain string formatter to clean up the OpenAPI document
+                options.OutputFormatters.RemoveType<StringOutputFormatter> ();
+
                 options.ReturnHttpNotAcceptable = true;
                 options.SuppressAsyncSuffixInActionNames = false;
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
             }
 
-            app.UseHttpsRedirection();
+            app.UseRouting ();
 
-            app.UseRouting();
+            app.UseCors (CorsPolicyName);
 
-            app.UseCors("AllowLocalAndAppServiceAngular");
+            app.UseAuthorization ();
 
-            app.UseAuthorization();
+            app.UseSwagger ();
 
-            app.UseSwagger();
-
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Guardian API V1");
+            app.UseSwaggerUI (c => {
+                c.SwaggerEndpoint ("/swagger/v1/swagger.json", "Guardian API V1");
             });
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ();
             });
         }
     }
