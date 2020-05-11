0acdd1d199143d898364b32761786416d89cdf53
Angelica Velez <anvelez26@gmail.com>
2020-05-11 16:08:57 -0500

    start of some tests

---
+++ TheGuardianAPI/TheGuardianAPI.Test/Core/HospitalsTest.cs
@@ -0,0 +1,13 @@
+using System;
+using TheGuardian.Core.Models;
+using Xunit;
+
+namespace TheGuardianAPI.Test.Core {
+    public class HospitalsTest {
+        [Fact]
+        public void Test1 () {
+
+        }
+
+    }
+}
