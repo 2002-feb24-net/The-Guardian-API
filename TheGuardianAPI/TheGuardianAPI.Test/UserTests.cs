ae4161517c1d53fff74baf9e03dc0ca748d99b3f
Russell Gehan <rdgehan@gmail.com>
2020-05-13 19:02:58 -0500

    Added more test methods to user tester

--- TheGuardianAPI/TheGuardianAPI.Test/UserTests.cs
+++ TheGuardianAPI/TheGuardianAPI.Test/UserTests.cs
@@ -10,19 +10,59 @@ using TheGuardian.Core.Interfaces;
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using TheGuardian.Core.Models;
+using TheGuardian.DataAccess;
 
 namespace TheGuardian.Test
 {
     public class UserTests
     {
-        Mock<IGuardianRepository> mockIRepo;
-        UsersController usersController;
+        private Mock<IGuardianRepository> mockIRepo;
+        private UsersController usersController;
 
         public UserTests() 
         {
             mockIRepo = new Mock<IGuardianRepository>();
             usersController = new UsersController(mockIRepo.Object);
         }
+
+        [Fact]
+        public async void PostUserTest()
+        {
+            DataAccess.User testSubject = new DataAccess.User
+            {
+                Id = 1,
+                FirstName = "Test",
+                LastName = "Subject",
+                Email = "testSubject@xunit.com",
+                Password = "password",
+                Address = "Address",
+                City = "City",
+                Zip = 12345,
+            };
+            mockIRepo.Setup(x => x.PostUserAsync(Mapper.MapUser(testSubject))).Verifiable();
+            var postResult = await usersController.PostUser(testSubject);
+            postResult.Result.Should().Equals(testSubject);
+        }
+
+        [Fact]
+        public async void PutUserTest()
+        {
+            DataAccess.User testSubject = new DataAccess.User
+            {
+                Id = 1,
+                FirstName = "Test",
+                LastName = "Subject 2",
+                Email = "testSubject@xunit.com",
+                Password = "password2",
+                Address = "Address2",
+                City = "City",
+                Zip = 12345,
+            };
+            mockIRepo.Setup(x => x.PutUserAsync(1, Mapper.MapUser(testSubject))).Verifiable();
+            var putResult = await usersController.PutUser(1, testSubject);
+            putResult.Should().Equals(testSubject);
+        }
+
 
         [Fact]
         public async void GetUsersTest()
@@ -41,25 +81,19 @@ namespace TheGuardian.Test
         }
 
         [Fact]
-        public async void PostDeleteUserTest()
+        public async void GetUserLoginTest()
         {
-            DataAccess.User testSubject = new DataAccess.User
-            {
-                Id = 1,
-                FirstName = "Test",
-                LastName = "Subject",
-                Email = "testSubect@xunit.com",
-                Password = "password",
-                Address = "Address",
-                City = "City",
-                Zip = 12345,
-            };
-            var postResult = await usersController.PostUser(testSubject);
-            postResult.Result.Should().Equals(testSubject);
+            mockIRepo.Setup(x => x.GetUserLoginAsync("testSubject@xunit.com", "password")).Verifiable();
+            var fetchedUser = await usersController.GetUser("testSjubect@xunit.com", "password");
+            fetchedUser.Result.Should().BeOfType<NotFoundResult>();
+        }
 
+
+        [Fact]
+        public async void DeleteUser()
+        {
             var deleteResult = await usersController.DeleteUser(1);
-            deleteResult.Result.Should().Equals(testSubject);
-
+            deleteResult.Result.Should().BeOfType<NotFoundResult>();
         }
     }
 }
